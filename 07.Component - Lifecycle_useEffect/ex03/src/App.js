3f73b49392a50c57a094d0d6bafa4c6c5ff3c692
seowonn <sonseowon5@gmail.com>
2025-02-12 17:46:37 +0900

    Clock 예제 finish

--- 07.Component - Lifecycle_useEffect/ex03/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex03/src/App.js
@@ -5,16 +5,37 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = this.__getCurrentTime();
     }
 
+    __getCurrentTime() {
+        const now = new Date();
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+            ticks: this.state ? this.state.ticks + 1 : 1
+        }
+    }
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState(this.__getCurrentTime());
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        console.log('App.componentWillUnmount');
+        clearInterval(this.intervalId);
+    }
     
     render() {
         return (
-                <Clock
-                    title={`ex03: Clock Component I:`}
-                    hours={'00'}
-                    minutes={'00'}
-                    seconds={'00'} />
+            <Clock 
+                title={`ex03: Clock Component I: ${this.state.ticks}`}
+                hours={this.state.hours}
+                minutes={this.state.minutes}
+                seconds={this.state.seconds} />
         )
     
     }
--- 07.Component - Lifecycle_useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex04/src/App.js
@@ -3,6 +3,37 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+
+    const getCurrentTime = () => {
+        const now = new Date();
+
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+        }
+    }
+
+    const [currentTime, setCurrentTime] = useState(getCurrentTime());
+    const [ticks, setTicks] = useState(0);
+
+    useEffect(() => {
+        console.log("App: 타이머 시작");
+
+        // prevTicks 이용: setState에 함수를 전달하면 React가 자동으로 제공하는
+        // 이전 상태 값
+        const intervalId = setInterval(() => {
+            setTicks(prevTicks => prevTicks + 1);
+        }, 1000);
+
+        return () => {
+            console.log("App 타이머 정리");
+            clearInterval(intervalId);
+        }
+    }, []);
+    // useEffect 함수의 두번째 인자에 [](빈 배열)을 넣으면 
+    // 처음 컴포넌트가 마운트(처음 렌더링) 될 때 한 번만 실행된다.
+
     return (
         <Clock
             title={`ex04: Clock Component II: ${ticks}`}
