c9c98f6090189207f3ad3ee4fb788df215a90012
seowonn <sonseowon5@gmail.com>
2025-02-13 10:25:53 +0900

    Fix: 시간 실시간 업데이트 수정, 설명 추가

--- 07.Component - Lifecycle_useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex04/src/App.js
@@ -17,18 +17,22 @@ export default function App() {
     const [currentTime, setCurrentTime] = useState(getCurrentTime());
     const [ticks, setTicks] = useState(0);
 
+    // 클래스형 컴포넌트의 componentDidMount() 함수와 같다.
+    // mount, unmount 시에는 두번째 인자로 빈 배열 넣기, 아니라면 계속 렌더링됨.
     useEffect(() => {
-        console.log("App: 타이머 시작");
+        console.log("App: mount될 때 타이머 시작");
 
         // prevTicks 이용: setState에 함수를 전달하면 React가 자동으로 제공하는
         // 이전 상태 값
         const intervalId = setInterval(() => {
             setTicks(prevTicks => prevTicks + 1);
+            setCurrentTime(getCurrentTime());
         }, 1000);
 
+        // unmount할 때 실행할 코드 작성
         return () => {
             console.log("App 타이머 정리");
-            clearInterval(intervalId);
+            clearInterval(intervalId); // clousure
         }
     }, []);
     // useEffect 함수의 두번째 인자에 [](빈 배열)을 넣으면 
